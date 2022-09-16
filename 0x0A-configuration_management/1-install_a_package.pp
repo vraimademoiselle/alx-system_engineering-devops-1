8bc0002cd229f830f03c76654f44d19be1247df5
Nevillionaire <75064256+nevillionaire@users.noreply.github.com>
2022-09-16 13:58:21 +0300

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,7 @@
-# Installing flask 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider =>  'pip3',
+# install puppet-lint
+# package
+
+package { 'puppet-lint':
+  ensure   => '2.1.1',
+  provider => 'gem'
 }
